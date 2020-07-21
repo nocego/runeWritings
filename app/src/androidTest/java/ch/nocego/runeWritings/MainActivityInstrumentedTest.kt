4d2cf5d721abe4edc3129598a3353a77199e44f2
nocego <44623630+nocego@users.noreply.github.com>
2020-07-22 00:45:52 +0200

    simplified code and removed runic resources

--- app/src/androidTest/java/ch/nocego/runeWritings/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/ch/nocego/runeWritings/MainActivityInstrumentedTest.kt
@@ -1,11 +1,18 @@
 package ch.nocego.runeWritings
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import ch.nocego.runeWritings.runes.LetterToRunes
+import org.junit.Before
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,8 +21,19 @@ import org.junit.Rule
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityInstrumentedtests {
+    lateinit var preferencesEditor: SharedPreferences.Editor
+    val ltr = LetterToRunes()
+    var targetContext: Context = getInstrumentation().targetContext
+
     @get:Rule
-    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+    var activityRule: ActivityTestRule<MainActivity> =
+        ActivityTestRule(MainActivity::class.java, true, false)
+
+    @Before
+    fun setUp() {
+        setDefaultSharedPrefsValues()
+        activityRule.launchActivity(Intent())
+    }
 
     @Test
     fun lettersInTitleByDefault() {
@@ -35,10 +53,35 @@ class MainActivityInstrumentedtests {
     @Test
     fun runesInTitleAfterSwitching() {
         moveSwitch()
-        onView(withId(R.id.mainActivityTitle)).check(matches(withText(R.string.titleRunic)))
+        val runicTranspilation = ltr.getRunesFromText(targetContext.getString(R.string.title))
+        onView(withId(R.id.mainActivityTitle)).check(matches(withText(runicTranspilation)))
+    }
+
+    @Test
+    fun runesInDescriptionAfterSwitching() {
+        moveSwitch()
+        val runicTranspilation =
+            ltr.getRunesFromText(targetContext.getString(R.string.appDescription))
+        onView(withId(R.id.description)).check(matches(withText(runicTranspilation)))
+    }
+
+    @Test
+    fun runesInSwitchRunesAfterSwitching() {
+        moveSwitch()
+        val runicTranspilation =
+            ltr.getRunesFromText(targetContext.getString(R.string.useRunesInsteadOfLetters))
+        onView(withId(R.id.switchRunes)).check(matches(withText(runicTranspilation)))
     }
 
     private fun moveSwitch() {
         onView(withId(R.id.switchRunes)).perform(click())
+    }
+
+    private fun setDefaultSharedPrefsValues() {
+        val sharedPrefs: SharedPreferences =
+            targetContext.getSharedPreferences("RUNES_INSTED_OF_LETTERS", Context.MODE_PRIVATE)
+        preferencesEditor = sharedPrefs.edit()
+        preferencesEditor.putBoolean("useRunes", false)
+        preferencesEditor.commit()
     }
 }
--- app/src/main/java/ch/nocego/runeWritings/MainActivity.kt
+++ app/src/main/java/ch/nocego/runeWritings/MainActivity.kt
@@ -1,20 +1,39 @@
 package ch.nocego.runeWritings
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import ch.nocego.runeWritings.runes.LetterToRunes
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val ltr = LetterToRunes()
+    var actionbar: ActionBar? = null
+    lateinit var appBarSwitch: MenuItem
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        actionbar = supportActionBar
+        actionbar!!.title = getText(R.string.title)
+
+        checkSwitch()
+        checkRunicTexts()
     }
 
     fun switchRunesToggle(v: View) {
-        changeTexts()
+        val editor = getSharedPrefs().edit()
+        editor.putBoolean("useRunes", switchRunes.isChecked)
+        editor.apply()
+        checkRunicTexts()
     }
 
     fun generateUnicodeRunesIntent(v: View) {
@@ -22,15 +41,36 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private fun changeTexts() {
-        changeTitle()
+    private fun getSharedPrefs() : SharedPreferences {
+        return getSharedPreferences("RUNES_INSTED_OF_LETTERS", Context.MODE_PRIVATE)
     }
 
-    private fun changeTitle() {
+    private fun checkSwitch() {
+        val switchState = getSharedPrefs().getBoolean("useRunes", false)
+        switchRunes.isChecked = switchState
+    }
+
+    private fun checkRunicTexts() {
+        transpileText(mainActivityTitle, R.string.title)
+        transpileText(description, R.string.appDescription)
+        transpileText(switchRunes, R.string.useRunesInsteadOfLetters)
+        transpileText(buttonGenerateUnicodeRunes, R.string.generateUnicodeRunes)
+        transpileActionBar(R.string.title)
+    }
+
+    private fun transpileText(tv: TextView, resourceId: Int) {
         if(switchRunes.isChecked){
-            mainActivityTitle.text = getString(R.string.titleRunic)
+            tv.text = ltr.getRunesFromText(getString(resourceId))
         }else{
-            mainActivityTitle.text = getString(R.string.title)
+            tv.text = getString(resourceId)
+        }
+    }
+
+    private fun transpileActionBar(resourceId: Int) {
+        if(switchRunes.isChecked){
+            actionbar!!.title = ltr.getRunesFromText(getString(resourceId))
+        }else{
+            actionbar!!.title = getString(resourceId)
         }
     }
 }
